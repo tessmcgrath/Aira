29d5bdb45ddd4d15257c104a5f79ee2de7b1051f
Peter McGrath <ppj.aira@gmail.com>
2025-07-19 06:36:03 +0100

    Added App Insights

---
+++ Strogue.Aira.Web/DependencyInjection.cs
@@ -0,0 +1,45 @@
+﻿namespace Strogue.Aira.Web;
+
+public static class DependencyInjection
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddApplicationInsightsTelemetry(new Microsoft.ApplicationInsights.AspNetCore.Extensions.ApplicationInsightsServiceOptions
+        {
+            ConnectionString = builder.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"]
+        });
+
+        builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
+        builder.Services.AddControllersWithViews();
+
+        return builder.Build();
+    }
+
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    {
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapStaticAssets();
+
+        app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}")
+            .WithStaticAssets();
+
+        //app.MapRazorPages()
+        //    .WithStaticAssets();
+
+        return app;
+    }
+}
